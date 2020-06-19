15d168f066e3d4eef109df9bfc3cc898de68e9e3
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-06-19 20:42:39 +0100

    details for every item displayed at their page

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,10 +2,11 @@ import React from 'react'
 import {View,Text} from 'react-native'
 import {stylesGlobal} from '../styles/global.js';
 const ReviewDetails = ({navigation}) => {
-
   return (
     <View style={stylesGlobal.container}>
-      <Text>Review Details page</Text>
+      <Text>{navigation.getParam('title')}</Text>
+      <Text>{navigation.getParam('body')}</Text>
+      <Text>{navigation.getParam('rating')}</Text>
     </View>
   )
 }
