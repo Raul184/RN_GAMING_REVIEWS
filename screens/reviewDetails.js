907c92bd927a1c2433044321511d3e5cdc65c040
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-06-22 21:29:42 +0100

    img for users'ratings attached

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,17 +1,32 @@
 import React from 'react'
-import {View,Text} from 'react-native'
-import {stylesGlobal} from '../styles/global.js';
+import {View,Text,StyleSheet,Image} from 'react-native'
+import {stylesGlobal , images} from '../styles/global.js';
 import Card from '../shared/Card'
+
 const ReviewDetails = ({navigation}) => {
+  const rating = navigation.getParam('rating')
   return (
     <View style={stylesGlobal.container}>
       <Card>
         <Text>{navigation.getParam('title')}</Text>
         <Text>{navigation.getParam('body')}</Text>
-        <Text>{navigation.getParam('rating')}</Text>
+        <View style={styles.ratings}>
+          <Text>Rating:</Text>
+          <Image source={images.ratings[rating]}/>
+        </View>
       </Card>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  ratings:{
+    flexDirection:'row',
+    justifyContent:'center',
+    paddingTop:16,
+    marginTop:16,
+    borderTopWidth:1,
+    borderTopColor:'#eee'
+  }
+})
 export default ReviewDetails;
